a23fc1f50e41638a080910603aa425ebfac9b616
Amin Atashpeykar <arash.atashpeykar@gmail.com>
2025-01-11 18:04:12 +0330

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react'
+import './App.css'
+
+export default function App() {
+
+
+  return (
+    <>
+      <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
