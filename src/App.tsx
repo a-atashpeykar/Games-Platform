e2c301e0f94126764dc116dc56973cab4ce2b84e
Amin Atashpeykar <arash.atashpeykar@gmail.com>
2025-01-11 18:18:38 +0330

    Build a nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {Grid, GridItem, Show} from '@chakra-ui/react'
 import './App.css'
+import NavBar from "./components/NavBar.tsx";
 
 export default function App() {
 
@@ -9,7 +10,9 @@ export default function App() {
           base: `"nav" "main"`,
           lg: `"nav nav" "aside main"`
       }}>
-          <GridItem area='nav' bg='coral'>Nav</GridItem>
+          <GridItem area='nav'>
+              <NavBar />
+          </GridItem>
           <Show above='lg'>
               <GridItem area='aside' bg='gold'>Aside</GridItem>
           </Show>
@@ -17,4 +20,5 @@ export default function App() {
       </Grid>
   )
 }
+
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+import {HStack, Image, Text} from "@chakra-ui/react";
+import logo from '../assets/red.png'
+export default function NavBar() {
+    return (
+        <HStack>
+            <Image src={logo} boxSize='50px' />
+            <Text>NavBar</Text>
+        </HStack>
+    )
+}
