5a2932de0f507cca0c4698bb6c3695d05c9e4e8e
Amin Atashpeykar <arash.atashpeykar@gmail.com>
2025-01-12 06:01:02 +0330

    Fetch genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,12 @@
+import useGenres from "../hooks/useGenres.ts";
+
+export default function GenreList() {
+    const { genres } = useGenres()
+    return (
+        <>
+            <ul>
+                {genres.map((genre) => <li key={genre.id}>{genre.name}</li>)}
+            </ul>
+        </>
+    )
+}
