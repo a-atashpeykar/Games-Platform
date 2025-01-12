6a720854128a8c52d234aa193e85f5406fde3f55
Amin Atashpeykar <arash.atashpeykar@gmail.com>
2025-01-12 04:41:43 +0330

    Create custom hook ro fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,25 +1,9 @@
-import {useEffect, useState} from "react";
-import apiClient from "../services/apiClient.ts";
 import {Text} from "@chakra-ui/react";
+import useGames from "../hooks/useGames.ts";
 
-
-interface Game{
-    id: number;
-    name: string;
-}
-interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-}
 export default function GameGrid() {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState('');
-    useEffect(() => {
-        apiClient.get<FetchGamesResponse>('/games')
-        .then((res) => setGames(res.data.results))
-        .catch((err) => setError(err.message));
-    }, [])
 
+    const {games, error} = useGames();
     return (
         <>
             {error && <Text>{error}</Text>}
